acce8482556841811447c5a21bf0c8423e1980c1
ABHISHEK PAL <abhishekpal370080@gmail.com>
2026-02-04 23:33:21 +0530

    Add files via upload

---
+++ BBDU.py
@@ -0,0 +1,164 @@
+"""x = int(input ("Enter the first number"))
+y = int(input ("Enter the second number"))
+sum = x+y 
+product = x*y
+difference = x-y
+print("sum , product, difference")"""
+
+"""a = int(input (Enter the first number))
+b = int(input (Enter the second number))
+sum= a+b
+diffrence= a-b
+product= a**b
+divide= a/b
+remainder= a%b
+
+print(division , remainder, power )"""
+
+# avarage 5 no.
+
+"""a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+d = int(input("Enter fourth number: "))
+e = int(input("Enter fifth number: "))
+
+total = a + b + c + d + e
+
+average = total / 5
+
+print("Average =", average)"""
+
+# shopkeaper
+"""pen_price = 15
+notebook_price = 40
+total_cost=(3*pen_price)+(2*notebook_price)
+print("total cost=", total_cost)"""
+
+# 5
+"""n= int(input("enter a number"))
+print("square:", n**2)
+print("cube:", n**3)
+print("square root:", n**0.5)"""
+
+#positive , negative, or zero
+
+"""num= int(input("enter a number:"))
+if num > 0:
+    print("the num is positive")
+elif num < 0:
+    print("the num is negative")
+
+else:
+ if num==0:
+    print("the num is zero")"""
+
+#voting system
+"""age= int(input("enter a number:"))
+if age>18:
+    print("you are eligible to vote")
+else:
+    print("you are eligible to vote")"""
+
+"""num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")"""
+
+"""a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("Greater number is:", a)
+elif b > a:
+    print("Greater number is:", b)
+else:
+    print("Both numbers are equal")"""
+
+"""num = int(input("Enter a number: "))
+
+if num % 5 == 0:
+    print("The number is divisible by 5")
+else:
+    print("The number is not divisible by 5")"""
+
+"""marks = int(input("Enter a number: "))
+if marks>=90:
+    print("grade A")
+elif marks>=75:
+    print("grade B")
+    
+elif marks>=50:
+    print("grade c")
+
+else:
+    print("fail")
+
+year = int(input("Enter a year: "))
+
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
+
+num = int(input("Enter the num: "))
+if num<10:
+    print("num is single digit")
+
+elif num <100:
+    print("num is double digit")
+
+else:
+    print("more than two digit")
+
+temp = float(input("Enter temperature in Celsius: "))
+
+if temp >= 0:
+    print("freezing")
+elif temp >= 20:
+    print("cold")
+
+elif temp >=35:
+    print("warm")
+
+else:
+    print("hot")
+
+num = int(input("Enter a number: "))
+
+if num > 0 and num % 2 == 0:
+    print("Positive and Even")
+elif num > 0 and num % 2 != 0:
+    print("Positive and Odd")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")"""
+
+password = input("Enter password: ")
+
+if password == "admin123":
+    print("Access Granted")
+else:
+    print("Access Denied")
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+    
